63a5271fe6c8f24558dacf32463030748417a729
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-10-10 16:26:21 +0700

    Add files via upload

---
+++ tugas_luas_lingkaran.php
@@ -0,0 +1,11 @@
+<?php
+$jari2 = 14;
+const phi = 22/7;
+
+function pow_luas($jari2, $pangkat){
+	return pow($jari2, $pangkat);
+}
+
+echo "<br> <h4>Tugas menghitung Luas Lingkaran</h4>";
+echo "<br> Jadi luas lingkaran adalah : ", number_format(2 * pow_luas($jari2, 2) , 2);
+?>
