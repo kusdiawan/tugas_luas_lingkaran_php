4abb3032fba7cd2e629d53376bc7d0e887c805b7
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-10-10 16:54:48 +0700

    Update tugas_luas_lingkaran.php

--- tugas_luas_lingkaran.php
+++ tugas_luas_lingkaran.php
@@ -2,10 +2,10 @@
 $jari2 = 14;
 const phi = 22/7;
 
-function pow_luas($jari2, $pangkat){
-	return pow($jari2, $pangkat);
+function luas_lingkaran($jari2){
+	return phi * ($jari2 * $jari2);
 }
 
 echo "<br> <h4>Tugas menghitung Luas Lingkaran</h4>";
-echo "<br> Jadi luas lingkaran adalah : ", number_format(phi * pow_luas($jari2, 2) , 2);
+echo "<br> Jadi luas lingkaran adalah : ", number_format(luas_lingkaran($jari2) , 2);
 ?>
